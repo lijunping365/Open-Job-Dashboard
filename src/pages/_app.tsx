32ef35237a146ef1661859ffdc5768bb3cd8c94a
lijunping006 <lijunping006@ke.com>
2023-12-06 18:50:47 +0800

    :sparkles: 主题色

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import '@/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Providers from '@/components/Provider';
+import { ConfigProvider } from 'antd';
 import { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
+import theme from '@/theme/themeConfig';
+import {AuthProvider} from "@/components/Provider/AuthProvider";
 
 export default function App({
   Component,
@@ -10,16 +13,16 @@ export default function App({
 }: AppProps) {
   return (
     <>
-      <Providers>
-        <ToastContainer
-          closeButton={false}
-          pauseOnFocusLoss={false}
-          toastClassName={() =>
-            'relative bg-white dark:bg-zinc-900 text-neutral-800 dark:text-white flex p-1 min-h-15 rounded-md justify-between overflow-hidden cursor-pointer p-5 border-2 dark:border-zinc-800 :dark:fill:slate-50 mb-4'
-          }
-        />
-        <Component {...pageProps} />
-      </Providers>
+      <ConfigProvider theme={theme}>
+        <AuthProvider>
+          <ToastContainer closeButton={false} pauseOnFocusLoss={false}
+            toastClassName={() =>
+              'relative bg-white dark:bg-zinc-900 text-neutral-800 dark:text-white flex p-1 min-h-15 rounded-md justify-between overflow-hidden cursor-pointer p-5 border-2 dark:border-zinc-800 :dark:fill:slate-50 mb-4'
+            }
+          />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ConfigProvider>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import HomeSEO from '@/seo/Home.seo';
 import Header from '@/components/Header';
-import Link from 'next/link';
 import Footer from '@/components/Footer';
 import BaseLayout from '@/components/Layout';
 
+import {Button} from 'antd'
 export default function HomePage() {
   return (
     <>
@@ -11,32 +11,9 @@ export default function HomePage() {
         <HomeSEO />
         <Header />
 
-        <div className='relative isolate bg-wash px-4 dark:bg-wash-dark lg:px-8'>
-          <div className='mx-auto max-w-2xl py-12 lg:py-24'>
-            <div className='text-center'>
-              <h1 className='text-4xl font-bold tracking-tight text-gray-800 dark:text-wash sm:text-6xl'>
-                Open-Idea
-              </h1>
-              <p className='mt-6 text-lg leading-8 text-gray-600 dark:text-wash'>
-                随时随地记录你的想法，轻松搭建私有知识库
-              </p>
-              <div className='mt-10 flex items-center justify-center gap-x-6'>
-                <Link
-                  href={`/space/home`}
-                  className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                >
-                  Get started
-                </Link>
-                <a
-                  href='https://openbytecode.com/project/open-idea'
-                  className='text-sm font-semibold leading-6 text-link dark:text-link-dark'
-                >
-                  Learn more <span aria-hidden='true'>→</span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Button type="primary">Button</Button>
+
+
 
         <Footer />
       </BaseLayout>
