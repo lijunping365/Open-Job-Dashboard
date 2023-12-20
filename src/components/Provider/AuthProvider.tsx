e6e56a06f0dae4944752c253b4b83b759a00d949
lijunping <2544054976@qq.com>
2023-12-20 22:25:37 +0800

    :sparkles: 修复token失效时未立即重定向到登录页bug

--- src/components/Provider/AuthProvider.tsx
+++ src/components/Provider/AuthProvider.tsx
@@ -2,12 +2,14 @@ import { AuthContext } from '@/components/Provider/AuthContext';
 import React, { useEffect, useState } from 'react';
 import { currentUser } from '@/services/api';
 import { removeAccessToken } from '@/lib/cache';
+import { useRouter } from 'next/navigation';
 
 export interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 export function AuthProvider(props: AuthProviderProps) {
+  const router = useRouter();
   const [user, setUser] = useState<any>();
 
   const initUser = async () => {
@@ -16,6 +18,7 @@ export function AuthProvider(props: AuthProviderProps) {
       if (user) setUser(user);
     } catch (error) {
       removeAccessToken();
+      router.replace('/');
     }
   };
 
