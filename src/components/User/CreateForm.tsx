f3c37e7eaa184d106f367ebea3526b01a835ca2c
lijunping <2544054976@qq.com>
2023-12-23 15:19:01 +0800

    :sparkles: 页面

--- src/components/User/CreateForm.tsx
+++ src/components/User/CreateForm.tsx
@@ -66,11 +66,11 @@ const CreateForm: React.FC<UpdateFormProps> = ({
         </FormItem>
 
         <FormItem
-          name='phone'
-          label='手机号'
-          rules={[{ required: true, message: '请输入手机号！' }]}
+          name='password'
+          label='密码'
+          rules={[{ required: true, message: '请输入密码！' }]}
         >
-          <Input placeholder='请输入手机号' />
+          <Input.Password placeholder='请输入密码' />
         </FormItem>
       </Form>
     </Modal>
