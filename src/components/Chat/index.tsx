3e8f706b194b9f02d19dff99b697649c7b4f1010
lijunping <2544054976@qq.com>
2024-01-04 08:22:44 +0800

    :sparkles: 优化样式

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -84,7 +84,14 @@ const AIChat = () => {
   };
 
   return (
-    <div style={{ height: '100vh', overflow: 'hidden' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        overflow: 'hidden',
+      }}
+    >
       <ChatList
         scrollRef={scrollRef}
         chatList={chatList}
