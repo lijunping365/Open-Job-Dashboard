2179c532f19261cbf81c475650b31873b46ba951
lijunping006 <lijunping006@ke.com>
2024-01-05 10:44:24 +0800

    :sparkles: chatModal

--- src/components/Chat/ChatModal.tsx
+++ src/components/Chat/ChatModal.tsx
@@ -2,19 +2,37 @@ import { FloatButton, Modal, notification } from 'antd';
 import AIChat from '@/components/Chat/index';
 import * as React from 'react';
 import { CommentOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 
 const ChatModal = () => {
-  const [api, contextHolder] = notification.useNotification();
-
-  const openNotification = () => {
+  const [api, contextHolder] = notification.useNotification({ top: 88 });
+  const [open, setOpen] = useState(false);
+  const openModal = () => {
     api.open({
-      message: 'Notification Title',
+      key: 'chat',
+      message: 'AI Chat',
       description: <AIChat />,
       duration: 0,
       style: {
         width: 500,
         height: 380,
       },
+      onClose: () => setOpen(false),
+    });
+  };
+
+  const closeModal = () => {
+    api.destroy('chat');
+  };
+
+  const handlerOpen = () => {
+    setOpen((prevState) => {
+      if (prevState) {
+        closeModal();
+      } else {
+        openModal();
+      }
+      return !prevState;
     });
   };
 
@@ -22,11 +40,12 @@ const ChatModal = () => {
     <>
       {contextHolder}
       <FloatButton.Group
+        open={open}
         trigger='click'
         type='primary'
         style={{ right: 24 }}
         icon={<CommentOutlined />}
-        onClick={() => openNotification()}
+        onClick={() => handlerOpen()}
       >
         {''}
       </FloatButton.Group>
