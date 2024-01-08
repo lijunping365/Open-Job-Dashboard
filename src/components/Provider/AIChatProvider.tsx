18ac88d42f88498ca667fcbe42e448c5c5d2ded8
lijunping <2544054976@qq.com>
2024-01-08 08:42:04 +0800

    :sparkles: 对话存储

--- src/components/Provider/AIChatProvider.tsx
+++ src/components/Provider/AIChatProvider.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { AIChatContext } from '@/components/Provider/AIChatContext';
 import ChatModal from '@/components/Chat/ChatModal';
 import { useRouter } from 'next/router';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { ChatItem } from '@/types/typings';
 
 export interface AIChatProviderProps {
   children: React.ReactNode;
@@ -12,19 +14,10 @@ const excludePath = ['/login', '/404'];
 export function AIChatProvider(props: AIChatProviderProps) {
   const router = useRouter();
   const cleanedPath = router.asPath.split(/[\?\#]/)[0];
-  const [chatList, setChatList] = useState<any>();
-
-  const initAIChat = async () => {
-    try {
-      // 初始化聊天数据
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    initAIChat().then();
-  }, []);
+  const [chatList, setChatList] = useLocalStorage<ChatItem[]>(
+    'open-job-ai',
+    []
+  );
 
   return (
     <AIChatContext.Provider value={{ chatList, setChatList }}>
