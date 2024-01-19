81e6cdff8941fafe8e9c3040540a70787e2fa2d9
lijunping <2544054976@qq.com>
2024-01-19 23:11:32 +0800

    :sparkles: AI chat

--- src/components/Chat/ChatModal.tsx
+++ src/components/Chat/ChatModal.tsx
@@ -96,6 +96,7 @@ const ChatModal = ({
         />
       ) : (
         <AIChat
+          chatConfig={chatConfig}
           cacheChatList={chatList}
           setCacheChatList={setChatList}
         />
