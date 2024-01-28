29ead9c051631dd6360dcfe57dd260e117f4f023
lijunping <2544054976@qq.com>
2024-01-28 11:12:37 +0800

    :sparkles: 修复 bug

--- src/components/Provider/AuthProvider.tsx
+++ src/components/Provider/AuthProvider.tsx
@@ -2,14 +2,12 @@ import { AuthContext } from '@/components/Provider/AuthContext';
 import React, { useEffect, useState } from 'react';
 import { currentUser } from '@/services/api';
 import { removeAccessToken } from '@/lib/cache';
-import { useRouter } from 'next/navigation';
 
 export interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 export function AuthProvider(props: AuthProviderProps) {
-  const router = useRouter();
   const [user, setUser] = useState<any>();
 
   const initUser = async () => {
@@ -18,7 +16,6 @@ export function AuthProvider(props: AuthProviderProps) {
       if (user) setUser(user);
     } catch (error) {
       removeAccessToken();
-      router.replace('/');
     }
   };
 
